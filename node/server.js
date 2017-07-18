80f7438591cb8acd04a287748b38d2c9529ed213
GLOBANT\martin.aguila <martin.aguila@globant.com>
2017-07-18 12:21:29 -0300

    node improving errors config service
    improved logic to avoid connection errors changin configuration by
    modifying the config file during load

--- node/server.js
+++ node/server.js
@@ -2,17 +2,33 @@
 var fs = require('fs');
 const http = require('http')  
 const port = 3000
+var counter = 0
+var change = 4000
+var max = 8000
 
 const requestHandler = (request, response) => {  
   console.log(request.url)
   response.writeHead(200, {"Content-Type": "application/json"});
   //var json = JSON.stringify({config: configObj});
   //var json = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-	fs.readFile('config.json', 'utf8', function (err, data) {
+  counter ++;
+  if (counter<change){
+	fs.readFile('config_close.json', 'utf8', function (err, data) {
 	  if (err) throw err;
 	  json = JSON.parse(data);
 	  response.end(JSON.stringify(json));
 	});
+  } else if (counter >= change){
+	fs.readFile('config_open.json', 'utf8', function (err, data) {
+	  if (err) throw err;
+	  json = JSON.parse(data);
+	  response.end(JSON.stringify(json));
+	});
+  } 
+  if (counter == max){
+	counter = 0;
+  }
+
   
   
 }
